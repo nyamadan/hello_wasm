5ba7fd4ff03877e4c49946c2b0bdde2b8449cf79
Yoshiki YAMADA <nyamadandan@gmail.com>
2023-02-18 17:56:55 +0900

    single file

--- main.c
+++ main.c
@@ -1,25 +1,22 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
+
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int *s_memory = NULL;
-
-int add(int a, int b);
+extern int add(int a, int b);
 
 void on_load(const char *filename) {
-  printf("onload: %s\n", filename);
+  printf("on_load: %s\n", filename);
   void *handle = dlopen(filename, RTLD_NOW);
   if (handle == NULL) {
     fprintf(stderr, "failed to dlopen\n");
     return;
   }
 
-  int (*add)(int, int);
-  add = dlsym(handle, "add");
-
+  int (*add)(int a, int b) = dlsym(handle, "add");
   if (add == NULL) {
     fprintf(stderr, "failed to dlsym\n");
     return;
@@ -31,6 +28,6 @@ void on_load(const char *filename) {
 void on_error(const char *error) { printf("onerror: %s\n", error); }
 
 int main() {
-  emscripten_async_wget("./build/side.wasm", "/side.wasm", on_load, on_error);
+  emscripten_async_wget("./build/side.wasm", "./side.wasm", on_load, on_error);
   return 0;
 }
